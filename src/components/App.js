5f00c7a3599313547a21a7d77a574bbfa1f2afe4
rupalisarcar <rupalisarcar@gmail.com>
2025-12-02 21:14:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 
 import React from "react";
 import './../styles/App.css';
+import ToDo from './index';
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div>        
+    <ToDo></ToDo>
     </div>
   )
 }
